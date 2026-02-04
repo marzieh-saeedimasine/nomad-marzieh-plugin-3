de597766e39d9a1feeb5da962217106e77343786
marzieh-saeedimasine <marzieh.saeedimasine@gmail.com>
2026-02-04 14:58:01 +0000

    Initial commit: Sintering plugin with schema and normalize

---
+++ src/nomad_sintering/schema_packages/__init__.py
@@ -0,0 +1,18 @@
+from nomad.config.models.plugins import SchemaPackageEntryPoint
+from pydantic import Field
+
+
+class SinteringEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from nomad_sintering.schema_packages.sintering import m_package
+
+        return m_package
+
+
+sintering = SinteringEntryPoint(
+    name='Sintering',
+    description='Schema package for describing a sintering process.',
+)
+
+__all__ = ['sintering']
